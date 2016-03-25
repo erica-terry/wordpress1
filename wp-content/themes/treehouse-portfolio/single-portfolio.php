ed3ea440ac68fd25c55819e7fe91e9cfa007d7e4
Erica Terry <eterry@knights.ucf.edu>
2016-03-25 11:47:10 -0400

    Created Porfiolio template

--- wp-content/themes/treehouse-portfolio/single-portfolio.php
+++ wp-content/themes/treehouse-portfolio/single-portfolio.php
@@ -6,23 +6,25 @@
       <!-- Primary Column -->
       <div class="small-12 medium-7 medium-offset-1 medium-push-4 columns">
         <div class="primary">
-                <?php if ( have_posts() ) : while( have_posts() ) : the_post(); ?>
-
-                <h1><?php the_title(); ?></h1>
-                <p><?php the_content(); ?></p>
-
-          </div>
+            <?php the_field('images'); ?>
         </div>
+      </div>
 
       <!-- Secondary Column -->
       <div class="small-12 medium-4 medium-pull-8 columns">
         <div class="secondary">
           <h1><?php the_title(); ?></h1>
-          
           <p><?php the_field('description'); ?></p>
+
+          <hr>
+
+          <p>
+            <?php previous_post_link(); ?> - 
+            <a href="<?php bloginfo(); ?>/portfolio">Back to Porfolio</a> - <?php next_post_link(); ?>
+          </p>
         </div>
       </div>
-      <?php endif; ?>
+      <?php endwhile; endif; ?>
     </div>
   </div>
 </section>
